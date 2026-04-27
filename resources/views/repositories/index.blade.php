36279240e55c9236f6bc48be9f431cec7a32b1fd
Alex Monroy <alexwebpuro@gmail.com>
2026-04-27 14:26:54 -0500

    Formulario de Edición con Diseño y Tests en Laravel

--- resources/views/repositories/index.blade.php
+++ resources/views/repositories/index.blade.php
@@ -29,10 +29,13 @@
                             <td class="border px-4 py-2">
                                 <a href={{ route('repositories.show', $repository) }}>Ver</a>
                             </td>
+                            <td class="border px-4 py-2">
+                                <a href={{ route('repositories.edit', $repository) }}>Editar</a>
+                            </td>
                         </tr>
                     @empty
                         <tr>
-                            <td colspan="3">No hay repositorios creados</td>
+                            <td colspan="4">No hay repositorios creados</td>
                         </tr>
                     @endforelse
                 </tbody>
--- tests/Feature/Http/Controllers/RepositoryControllerTest.php
+++ tests/Feature/Http/Controllers/RepositoryControllerTest.php
@@ -80,6 +80,31 @@ class RepositoryControllerTest extends TestCase
             ->get("repositories/$repository->id")
             ->assertStatus(403);
     }
+
+    public function test_edit(): void
+    {
+        $user = User::factory()->create();
+        $repository = Repository::factory()->create(['user_id' => $user->id]);
+        
+        // $this->withoutExceptionHandling();
+        $this
+            ->actingAs($user)
+            ->get("repositories/$repository->id/edit")
+            ->assertStatus(200)
+            ->assertSee($repository->url)
+            ->assertSee($repository->description);
+    }
+
+    public function test_edit_policy(): void
+    {
+        $user = User::factory()->create(); // id = 1
+        $repository = Repository::factory()->create(); // user_id = 2
+
+        $this
+            ->actingAs($user)
+            ->get("repositories/$repository->id/edit")
+            ->assertStatus(403);
+    }
     
     
     public function test_store(): void
