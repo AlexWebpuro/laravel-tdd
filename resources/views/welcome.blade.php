d71050d7aaabe9d759e0a8e74c590256013f69f5
Alex Monroy <alexwebpuro@gmail.com>
2026-05-04 16:32:02 -0500

    Diseño Web con Tailwind CSS: Mejora de Apariencia y Formato.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,9 +20,23 @@
         </style>
     </head>
     <body class="bg-gray-200">
-        @foreach ($repositories as $repository )
-        <h2>{{ $repository->url }}</h2>
-        <p>{{ $repository->description }}</p>
-        @endforeach
+        <ul class="max-w-lg bg-white border-r border-gray-300 shadow-xl">
+            @foreach ($repositories as $repository )
+            <li class="flex items-center text-black p-2 hover:gb-gray-300">
+                <img 
+                    src="{{ $repository->user->profile_photo_url }}" 
+                    class="w-12 h-12 rounded-full mr-2" alt="">
+                <div class="flex justify-between w-full">
+                    <div class="flex-1 ">
+                        <h2 class="text-sm font-semibold text-black">{{ $repository->url }}</h2>
+                        <p class="">{{ $repository->description }}</p>
+                    </div>
+                    <span class="text-xs font-medium text-gray-600">
+                        {{ $repository->create_at }}
+                    </span>
+                </div>
+            </li>
+            @endforeach
+        </ul>
     </body>
 </html>
